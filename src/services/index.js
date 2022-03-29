be650a431e39cef09467d85a9801e05616a900ab
starcictoni <57995236+starcictoni@users.noreply.github.com>
2022-03-29 16:10:23 +0200

    Quick cleanup

--- src/services/index.js
+++ src/services/index.js
@@ -4,12 +4,7 @@ let Service = {
     async getGlobal(numb) {
         let response = await axios.get(`https://api.github.com/repos/vuejs/vue/commits?per_page=${numb}`)
         return response.status === 200 ? response.data : 'No available data';
-        
     },
-    async getDetails(sha) {
-        let response = await axios.get(`https://api.github.com/repos/vuejs/vue/commits/${sha}`)
-        return response.status === 200 ? response.data : 'No available data';
-    }
 }
 
 export default Service;
